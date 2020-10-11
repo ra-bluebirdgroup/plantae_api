2d0d181511cc596d9ca53ecc2e18b8ea699b89d2
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-10-11 14:25:12 -0400

    pg

--- db/migrate/20200807003731_create_plants.rb
+++ db/migrate/20200807003731_create_plants.rb
@@ -1,7 +1,7 @@
 class CreatePlants < ActiveRecord::Migration[6.0]
   def change
     create_table :plants do |t|
-      t.string :scientific_name
+      t.text :scientific_name
       t.timestamps
     end
   end
