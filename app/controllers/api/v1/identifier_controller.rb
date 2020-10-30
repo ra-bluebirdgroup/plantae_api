e8b7785df3997ecc670b481730d78301dc94d673
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-10-30 00:16:12 -0400

    GV to PID switch

--- app/controllers/api/v1/identifier_controller.rb
+++ app/controllers/api/v1/identifier_controller.rb
@@ -6,38 +6,21 @@ class Api::V1::IdentifierController < ApplicationController
 skip_before_action :authorized, only: [:getImageId]
 
   def getImageId
-    # Instantiates a client
-    image_annotator = Google::Cloud::Vision.image_annotator
+    key = {
+        api_key: ENV['PLANTID'],
+        images: params[:imagePath],
+        modifiers: ["crops_fast", "similar_images"],
+        plant_language: "en",
+        plant_details: ["common_names",
+                          "url",
+                          "name_authority",
+                          "wiki_description",
+                          "taxonomy",
+                          "synonyms"]
+      };
 
-    # The name of the image file to annotate
-    file_name = "#{params[:imagePath]}"
-    response = {}
-    descriptions = []
-    # Performs label detection on the image file
-    vision_response = image_annotator.label_detection image: file_name
-    vision_response.responses.each do |res|
-      res.label_annotations.each do |label|
-      p label
-      descriptions.push("#{label.description}")
-
-      end
-    end
-
-    plants = []
-
-    descriptions.each do |desc|
-      trefleresponse = RestClient.get("https://trefle.io/api/v1/plants/search?token=#{ENV["TREFLE_API_KEY"]}&q=#{desc}")
-      data = JSON.parse(trefleresponse)
-        if data["data"].length > 0
-         plants.push(data["data"])
-       end
-
-      end
-
-    response["descriptions"] = descriptions
-
-    response["plants"] = plants
-    render json: response
+RestClient.post "https://api.plant.id/v2/identify", key.to_json, {content_type: :json, accept: :json}
+data = JSON.parse(response)
   end
 
 end
